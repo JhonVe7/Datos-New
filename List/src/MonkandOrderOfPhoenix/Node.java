6884cf52ed5b4d46ff06a15269f64ec69a88f84e
JhonVe7 <jvelascoriv@uniminuto.edu.co>
2018-12-04 09:19:51 -0500

    add excersice

--- List/src/MonkandOrderOfPhoenix/Node.java
+++ List/src/MonkandOrderOfPhoenix/Node.java
@@ -1,5 +1,29 @@
 package MonkandOrderOfPhoenix;
+import MonkandOrderOfPhoenix.Node;
 
-public class Node {
+public class Node{//class Node
 
+	//Student
+	public int escuela;
+	public int pergamino;
+	
+	
+	public Node next; //pointer
+	
+	public Node() {}//Constructor
+	
+	public Node(int School, int pergamino)
+	{//beginning constructor
+		
+		this.escuela = School;
+		this.pergamino = pergamino;
+		
+		
+	}//end Initialization
+	
+	public String toString() {//method print list
+		
+		return this.escuela + "" + this.pergamino + " ";
+		
+	}
 }
--- List/src/MonkandPrisionerofAzckaban/MonkandAzkaban.java
+++ List/src/MonkandPrisionerofAzckaban/MonkandAzkaban.java
@@ -12,85 +12,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-
 public class MonkandAzkaban {
-		
+
 	public static void main(String[] args) throws IOException {
-			
-		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
-			
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int X = 0;
 		int Y = 0;
-		
+
 		String Quantity = br.readLine();
-		
-				
+
 		String Num = br.readLine();
-				
+
 		String Nums[] = Num.split(" ");
-				
-					
+
 		int array[] = new int[Integer.parseInt(Quantity)];
-				
-					
+
 		for (int i = 0; i < Integer.parseInt(Quantity); i++) {
-					
-					
+
 			array[i] = Integer.parseInt(Nums[i]);
-					
-			
+
 		}
-					
+
 		for (int n = 0; n < Integer.parseInt(Quantity); n++) {
-			
+
 			for (int i = n; i < Integer.parseInt(Quantity); i++) {
-						
-				if(i+1 == Integer.parseInt(Quantity)) {
+
+				if (i + 1 == Integer.parseInt(Quantity)) {
 					X = -1;
 					break;
 				}
-						
-				if(array[i+1] > array[n]) {					
-					X = (i+2);
-					break;					
-				}
-				else 
-							
-					X = -1;			
-								
+
+				if (array[i + 1] > array[n]) {
+					X = (i + 2);
+					break;
+				} else
+
+					X = -1;
+
 			}
-			
-			for (int j = n; j>=0; j--) {
-						
-				if(j-1 == -1) {
+
+			for (int j = n; j >= 0; j--) {
+
+				if (j - 1 == -1) {
 					Y = -1;
 					break;
 				}
-				
-				if(array[j-1] > array[n]) {
-							
+
+				if (array[j - 1] > array[n]) {
+
 					Y = (j);
-								
+
 					break;
-							
-				}
-				else 
-					
-					Y = -1;		
-						
+
+				} else
+
+					Y = -1;
+
 			}
-					
-			int Total = (X)+(Y);
-						
+
+			int Total = (X) + (Y);
+
 			System.out.println(Total);
-					
-			X=0;
-			Y=0;
-					
+
+			X = 0;
+			Y = 0;
+
 		}
-			
+
 	}
-		
-		
+
 }
